de9713a11ab301da516b69a11a5282b094a72181
wako-mori <63290071+wako-mori@users.noreply.github.com>
2020-06-30 12:36:32 +0900

    haml,scssへ変更（一からやり直し）

--- db/migrate/20200626033308_create_posts.rb
+++ db/migrate/20200626033308_create_posts.rb
@@ -1,9 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.integer :rank
-      t.references :user, foreign_key: true
-      t.integer :score
+      t.integer :point
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,9 @@
 ActiveRecord::Schema.define(version: 2020_06_26_033308) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "rank"
-    t.bigint "user_id"
-    t.integer "score"
+    t.integer "point"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -33,5 +30,4 @@ ActiveRecord::Schema.define(version: 2020_06_26_033308) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "posts", "users"
 end
